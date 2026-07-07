3061d1b7390545392b5d8734cd3721f5f62cac56
lorenzo.tanganelli@sisal.it <lorenzo.tanganelli@sisal.it>
2026-07-07 17:21:49 +0200

    add new channels

--- aggiorna_dati.sh
+++ aggiorna_dati.sh
@@ -31,8 +31,8 @@ find dati_grezzi -mindepth 1 -delete
 find letture_pulite -mindepth 1 -delete
 
 echo "[3/4] Raccolta dati con yt-dlp..."
-yt-dlp --playlist-end 20 --ignore-errors --no-download --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@pingpongstyles"
-yt-dlp --playlist-end 20 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@Fitetofficial"
+yt-dlp --playlist-end 25 --ignore-errors --no-download --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@pingpongstyles"
+yt-dlp --playlist-end 40 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@Fitetofficial"
 yt-dlp --playlist-end 50 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@wttglobal"
 yt-dlp --playlist-end 10 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@MilanoSportTT"
 yt-dlp --playlist-end 10 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@YouPongOfficial"
@@ -40,6 +40,15 @@ yt-dlp --playlist-end 10 --ignore-errors --no-download -t sleep --write-info-jso
 yt-dlp --playlist-end 10 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@GiacomoCerea"
 yt-dlp --playlist-end 10 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@FilippoCantellaTT"
 yt-dlp --playlist-end 10 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@mitsutabletennis"
+
+yt-dlp --playlist-end 1000 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@Top8TT"
+yt-dlp --playlist-end 1000 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@TableSkills"
+yt-dlp --playlist-end 1000 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@LucaLaNotteTTplayer"
+yt-dlp --playlist-end 1000 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@TableTennisDaily"
+yt-dlp --playlist-end 1000 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@tabletennisindependent3737"
+yt-dlp --playlist-end 1000 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@TTtrix"
+yt-dlp --playlist-end 1000 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@BeyondThePodiumOfficial"
+yt-dlp --playlist-end 1000 --ignore-errors --no-download -t sleep --write-info-json --output "dati_grezzi/%(id)s" "https://www.youtube.com/@giacomoizzo2007"
 
 
 echo "[4/5] Elaborazione dati con ytp.py..."
